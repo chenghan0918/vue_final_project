7d5b9de236ad7e07ff85b221da207ba39b0d4d41
Leo <chenghan0918@gmail.com>
2021-01-06 10:22:50 +0800

    axios環境建置與api串接

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <router-view></router-view>
   </div>
 </template>
+<script>
+export default{
+  created(){
+    var api = 'https://vue-course-api.hexschool.io/api/chenghan0918/products'
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
+  }
+}
+</script>
 <style lang="scss">
 @import "./assets/all"
 </style>
