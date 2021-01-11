dab96aaa4784328fb270e017765c96fcc91b0f5d
Leo <chenghan0918@gmail.com>
2021-01-11 10:11:31 +0800

    bootstrap modal 串接成功(版本4)

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productModal">建立新產品</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#productModal">
+                建立新產品
+            </button>
         </div>
         <table class="table mt-4">
             <thead>
@@ -34,19 +36,22 @@
                 </tr>
             </tbody>
         </table>
+      
         <!-- Modal -->
-<div class="modal fade" id="productModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="productModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
       <div class="modal-body">
         ...
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
